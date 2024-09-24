64ed52e704fee8c6922e8e5cc2b9d670b871af48
Charrity <ntswakimohale@gmail.com>
2024-09-24 21:38:44 +0200

    Updated Time zones
    added id of city on select, added timezones on values, added a function to update city on javascript

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 setInterval(function () {
-  let capetownElement = document.querySelector("#cape-town");
-  let capetownDateElement = capetownElement.querySelector(".date");
-  let capetownTimeElement = capetownElement.querySelector(".time");
-  capetownDateElement.innerHTML = moment().format("Do MMMM YYYY");
-  capetownTimeElement.innerHTML = moment()
-    .tz("Africa/Cape_Town")
+  let sydneyElement = document.querySelector("#sydney");
+  let sydneyDateElement = sydneyElement.querySelector(".date");
+  let sydneyTimeElement = sydneyElement.querySelector(".time");
+  sydneyDateElement.innerHTML = moment().format("Do MMMM YYYY");
+  sydneyTimeElement.innerHTML = moment()
+    .tz("Australia/Sydney")
     .format("H:mm:ss [<small>]A[</small>]");
 
   let gaboroneElement = document.querySelector("#gaborone");
@@ -15,3 +15,23 @@ setInterval(function () {
     .tz("Africa/Gaborone")
     .format("H:mm:ss [<small>]A[</small>]");
 }, 1000);
+
+function updateCity(event) {
+    let cityTimezone = event.target.value;
+    let cityName = cityTimezone.replace("_", "").split("/")[1];
+    let cityTime = moment().tz(cityTimezone);
+    let citiesElement = document.querySelector("#cities");
+    citiesElement.innerHTML = `<div class="city" id="gaborone">
+    <div>
+<h2>${cityName}</h2>
+<div class="date">${cityTime.format("Do MMMM YYYY")}</div>
+</div>
+<div class="time"> ${cityTime.format("H:mm:ss")} <small>${cityTime.format(
+      "A"
+    )}</small></div>
+</div>
+ </div>
+    </div>`;
+}
+let citiesSelect = document.querySelector("#city");
+citiesSelect.addEventListener("change", updateCity);
